65b66d9f090c4d858e257e7f30311133627c6115
Gwen Hudson <hudson444@yahoo.com>
2018-12-20 16:41:33 -0600

    change index

---
+++ checkpoint-three/script.js
@@ -0,0 +1,23 @@
+//Project 3 
+'use strict';
+//alert("Color Wheel Lesson")
+
+$(document).ready(function(){
+  $("button1").click(function(){
+    $("#violet").toggle();
+  });
+});
+
+
+$(document).ready(function(){
+  $("button2").click(function(){
+    $("#green").toggle();
+  });
+});
+
+
+$(document).ready(function(){
+  $("button3").click(function(){
+    $("#orange").toggle();
+  });
+});
